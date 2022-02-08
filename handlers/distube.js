5312661b21d6f3492a6c0856879da17503be1189
dewstouh <53043542+dewstouh@users.noreply.github.com>
2022-02-08 16:13:14 +0100

    Add files via upload

---
+++ handlers/distube.js
@@ -0,0 +1,59 @@
+const {DisTube} = require('distube');
+const { SpotifyPlugin } = require('@distube/spotify');
+const { SoundCloudPlugin } = require('@distube/soundcloud');
+module.exports = (client, Discord) => {
+    console.log(`Modulo de MÚSICA Cargado!`.red)
+
+    client.distube = new DisTube(client, {
+        emitNewSongOnly: false,
+        leaveOnEmpty: true,
+        leaveOnFinish: true,
+        leaveOnStop: true,
+        savePreviousSongs: true,
+        emitAddSongWhenCreatingQueue: false,
+        searchSongs: 0,
+        nsfw: false,
+        emptyCooldown: 25,
+        ytdlOptions: {
+            highWaterMark: 1024 * 1024 * 64,
+            quality: "highestaudio",
+            format: "audioonly",
+            liveBuffer: 60000,
+            dlChunkSize: 1024 * 1024 * 4,
+        },
+        youtubeDL: false,
+        plugins: [
+            new SpotifyPlugin({
+                parallel: true,
+                emitEventsAfterFetching: true,
+            }),
+            new SoundCloudPlugin()
+        ],
+    });
+
+    //escuchamos los eventos de DisTube
+
+    client.distube.on("playSong", (queue, song) => {
+        queue.textChannel.send({
+            embeds: [new Discord.MessageEmbed()
+            .setTitle(`Reproduciendo \`${song.name}\` - \`${song.formattedDuration}\``)
+            .setThumbnail(song.thumbnail)
+            .setURL(song.url)
+            .setColor("#8400ff")
+            .setFooter({text: `Añadida por ${song.user.tag}`, iconURL: song.user.displayAvatarURL({dynamic: true})})
+            ]
+        })
+    })
+
+    client.distube.on("addSong", (queue, song) => {
+        queue.textChannel.send({
+            embeds: [new Discord.MessageEmbed()
+            .setTitle(`✅ Añadido \`${song.name}\` - \`${song.formattedDuration}\``)
+            .setThumbnail(song.thumbnail)
+            .setURL(song.url)
+            .setColor("#8400ff")
+            .setFooter({text: `Añadida por ${song.user.tag}`, iconURL: song.user.displayAvatarURL({dynamic: true})})
+            ]
+        })
+    })
+};
