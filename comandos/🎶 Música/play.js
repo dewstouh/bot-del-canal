6b8a6b23412c8c5591c8a9872d64603b7c7cfaf8
dewstouh <53043542+dewstouh@users.noreply.github.com>
2022-02-08 16:13:23 +0100

    Add files via upload

---
+++ comandos/🎶 Música/play.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "play",
+    aliases: ["reproducir"],
+    desc: "Sirve para reproducir una canción",
+    run: async (client, message, args, prefix) => {
+        //comprobaciones previas
+        if(!args.length) return message.reply(`❌ **Tienes que especificar el nombre de una canción!**`);
+        if(!message.member.voice?.channel) return message.reply(`❌ **Tienes que estar en un canal de voz para ejecutar este comando!**`);
+        if(message.guild.me.voice?.channel && message.member.voice?.channel.id != message.guild.me.voice?.channel.id) return message.reply(`❌ **Tienes que estar en el mismo canal de voz __QUE YO__ para ejecutar este comando!**`);
+        client.distube.play(message.member.voice?.channel, args.join(" "), {
+            member: message.member,
+            textChannel: message.channel,
+            message
+        });
+        message.reply(`🔎 **Buscando \`${args.join(" ")}\`...**`);
+    }
+}
