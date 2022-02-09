b75340faca19d407a2b536255498e74e5e827fe3
dewstouh <53043542+dewstouh@users.noreply.github.com>
2022-02-09 15:19:17 +0100

    #4 Update
    Añadidos skip y stop

---
+++ comandos/🎶 Música/skip.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "skip",
+    aliases: ["saltar"],
+    desc: "Sirve para saltar una canción",
+    run: async (client, message, args, prefix) => {
+        //comprobaciones previas
+        const queue = client.distube.getQueue(message);
+        if(!queue) return message.reply(`❌ **No hay ninguna canción reproduciéndose!**`);
+        if(!message.member.voice?.channel) return message.reply(`❌ **Tienes que estar en un canal de voz para ejecutar este comando!**`);
+        if(message.guild.me.voice?.channel && message.member.voice?.channel.id != message.guild.me.voice?.channel.id) return message.reply(`❌ **Tienes que estar en el mismo canal de voz __QUE YO__ para ejecutar este comando!**`);
+        client.distube.skip(message);
+        message.reply(`⏭ **Saltando a la siguiente canción!**`)
+    }
+}
---
+++ comandos/🎶 Música/stop.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "stop",
+    aliases: ["desconectar", "leavevc", "leave", "disconnect"],
+    desc: "Sirve para desconectar al bot de la sala de voz",
+    run: async (client, message, args, prefix) => {
+        //comprobaciones previas
+        const queue = client.distube.getQueue(message);
+        if(!queue) return message.reply(`❌ **No hay ninguna canción reproduciéndose!**`);
+        if(!message.member.voice?.channel) return message.reply(`❌ **Tienes que estar en un canal de voz para ejecutar este comando!**`);
+        if(message.guild.me.voice?.channel && message.member.voice?.channel.id != message.guild.me.voice?.channel.id) return message.reply(`❌ **Tienes que estar en el mismo canal de voz __QUE YO__ para ejecutar este comando!**`);
+        client.distube.stop(message);
+        message.reply(`🏃‍♂️ **Desconectado!**`)
+    }
+}
