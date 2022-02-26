20434789bc1c9adcb59b5d269bf3c1a62ca1da79
dewstouh <53043542+dewstouh@users.noreply.github.com>
2022-02-26 23:28:10 +0100

    Elimina código inútil/ Optimizado
    Ya no se hace uso de la función asegurar() ya que se usa asegurar_todo() y se arregla el bug de la doble base de datos

--- eventos/guild/messageCreate.js
+++ eventos/guild/messageCreate.js
@@ -1,13 +1,11 @@
 const config = require(`${process.cwd()}/config/config.json`)
 const serverSchema = require(`${process.cwd()}/modelos/servidor.js`)
-const { asegurar, asegurar_todo } = require(`${process.cwd()}/handlers/funciones.js`)
+const { asegurar_todo } = require(`${process.cwd()}/handlers/funciones.js`)
 module.exports = async (client, message) => {
     if (!message.guild || !message.channel || message.author.bot) return;
-    let data = await asegurar(serverSchema, "guildID", message.guild.id, {
-        guildID: message.guild.id,
-        prefijo: config.prefix
-    });
-    await asegurar_todo(message.guild.id)
+    //aseguramos la base de datos en caso de que no haya una creada para el servidor
+    await asegurar_todo(message.guild.id);
+    let data = await serverSchema.findOne({guildID: message.guild.id});
     if (!message.content.startsWith(data.prefijo)) return;
     const args = message.content.slice(data.prefijo.length).trim().split(" ");
     const cmd = args.shift()?.toLowerCase();
