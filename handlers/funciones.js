6281649a1454c13947451facc2d7671ec332e114
dewstouh <53043542+dewstouh@users.noreply.github.com>
2022-02-26 23:23:20 +0100

    Eliminado Código intuil / Optimizado
    Eliminada la función asegurar() ya que no era necesaria

--- handlers/funciones.js
+++ handlers/funciones.js
@@ -3,18 +3,7 @@ const setupSchema = require(`${process.cwd()}/modelos/setups.js`);
 const config = require(`${process.cwd()}/config/config.json`);
 
 module.exports = {
-    asegurar,
     asegurar_todo,
-}
-
-async function asegurar(schema, id, id2, objeto){
-    let data = await schema.findOne({id: id2})
-    if(!data){
-        console.log("NO HABIA BASE DE DATOS CREADA, CREANDO UNA...")
-        data = await new schema(objeto)
-        data.save();
-    }
-    return data;
 }
 
 async function asegurar_todo(guildid){
