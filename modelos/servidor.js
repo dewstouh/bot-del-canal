0cac29932f0adb7249e76c5766e4942edb947345
dewstouh <53043542+dewstouh@users.noreply.github.com>
2022-03-22 18:32:34 +0100

    Add files via upload

--- modelos/servidor.js
+++ modelos/servidor.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const serverSchema = new mongoose.Schema({
     guildID: String,
     prefijo: String,
-    premium: {type: String, default: ""}
+    premium: {type: String, default: ""},
+    idioma: {type: String, default: "es"},
 })
 
 const model = mongoose.model("ConfigServer", serverSchema);
